941daf841562ca4a030fe1828a5cab698ba22b63
ErodeesFleurs <862959461@qq.com>
2024-07-05 15:37:53 +0800

    refactor: Remove commented out code in executor_update.h and sm_manager.cpp

--- src/execution/executor_update.h
+++ src/execution/executor_update.h
@@ -41,10 +41,10 @@ class UpdateExecutor : public AbstractExecutor {
         rids_ = rids;
         context_ = context;
 
-        if (context_->txn_ != nullptr) {
-            context_->lock_mgr_->lock_IX_on_table(
-                context->txn_, sm_manager_->fhs_[tab_name_]->GetFd());
-        }
+        // if (context_->txn_ != nullptr) {
+        //     context_->lock_mgr_->lock_IX_on_table(
+        //         context->txn_, sm_manager_->fhs_[tab_name_]->GetFd());
+        // }
         std::cerr << "UpdateExecutor::UpdateExecutor() should be called end"
                   << std::endl;
     }
