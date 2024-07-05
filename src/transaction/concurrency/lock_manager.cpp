2d0f479d11a38a6bfac35aa87574b3e7f4add96d
ErodeesFleurs <862959461@qq.com>
2024-07-05 15:56:46 +0800

    refactor: Remove commented out code, fix failure handling, and add debug prints for transaction management

--- src/transaction/concurrency/lock_manager.cpp
+++ src/transaction/concurrency/lock_manager.cpp
@@ -19,25 +19,25 @@ See the Mulan PSL v2 for more details. */
  */
 bool LockManager::lock_shared_on_record(Transaction* txn, const Rid& rid,
                                         int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, rid, LockDataType::RECORD);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(), LockMode::SHARED);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, rid, LockDataType::RECORD);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(), LockMode::SHARED);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
 
-    if (!WaitDie(request_queue, request)) {
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (!WaitDie(request_queue, request)) {
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -51,25 +51,25 @@ bool LockManager::lock_shared_on_record(Transaction* txn, const Rid& rid,
  */
 bool LockManager::lock_exclusive_on_record(Transaction* txn, const Rid& rid,
                                            int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, rid, LockDataType::RECORD);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(), LockMode::EXLUCSIVE);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, rid, LockDataType::RECORD);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(), LockMode::EXLUCSIVE);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
 
-    if (!WaitDie(request_queue, request)) {
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (!WaitDie(request_queue, request)) {
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -81,25 +81,25 @@ bool LockManager::lock_exclusive_on_record(Transaction* txn, const Rid& rid,
  * @param {int} tab_fd 目标表的fd
  */
 bool LockManager::lock_shared_on_table(Transaction* txn, int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(), LockMode::SHARED);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(), LockMode::SHARED);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
 
-    if (!WaitDie(request_queue, request)) {
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (!WaitDie(request_queue, request)) {
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -111,25 +111,25 @@ bool LockManager::lock_shared_on_table(Transaction* txn, int tab_fd) {
  * @param {int} tab_fd 目标表的fd
  */
 bool LockManager::lock_exclusive_on_table(Transaction* txn, int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(), LockMode::EXLUCSIVE);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(), LockMode::EXLUCSIVE);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
 
-    if (!WaitDie(request_queue, request)) {
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (!WaitDie(request_queue, request)) {
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -140,25 +140,25 @@ bool LockManager::lock_exclusive_on_table(Transaction* txn, int tab_fd) {
  * @param {int} tab_fd 目标表的fd
  */
 bool LockManager::lock_IS_on_table(Transaction* txn, int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(), LockMode::INTENTION_SHARED);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(), LockMode::INTENTION_SHARED);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
 
-    if (!WaitDie(request_queue, request)) {
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (!WaitDie(request_queue, request)) {
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -170,28 +170,28 @@ bool LockManager::lock_IS_on_table(Transaction* txn, int tab_fd) {
  * @param {int} tab_fd 目标表的fd
  */
 bool LockManager::lock_IX_on_table(Transaction* txn, int tab_fd) {
-    std::unique_lock<std::mutex> lock(latch_);
-    LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
-    auto& request_queue = lock_table_[lock_data_id];
-    LockRequest request(txn->get_transaction_id(),
-                        LockMode::INTENTION_EXCLUSIVE);
-    request_queue.request_queue_.push_back(request);
+    // std::unique_lock<std::mutex> lock(latch_);
+    // LockDataId lock_data_id(tab_fd, LockDataType::TABLE);
+    // auto& request_queue = lock_table_[lock_data_id];
+    // LockRequest request(txn->get_transaction_id(),
+    //                     LockMode::INTENTION_EXCLUSIVE);
+    // request_queue.request_queue_.push_back(request);
 
-    if (txn->get_state() == TransactionState::DEFAULT) {
-        txn->set_state(TransactionState::GROWING);
-    }
-    std::cerr << "lock_IX_on_table: wait die: "
-              << WaitDie(request_queue, request) << std::endl;
-    if (!WaitDie(request_queue, request)) {
-        std::cerr << "lock_IX_on_table: grant lock" << std::endl;
-        while (!GrantLock(request_queue, request)) {
-            request_queue.cv_.wait(lock);
-        }
-        txn->append_lock_set(lock_data_id);
-    } else {
-        throw TransactionAbortException(txn->get_transaction_id(),
-                                        AbortReason::DEADLOCK_PREVENTION);
-    }
+    // if (txn->get_state() == TransactionState::DEFAULT) {
+    //     txn->set_state(TransactionState::GROWING);
+    // }
+    // std::cerr << "lock_IX_on_table: wait die: "
+    //           << WaitDie(request_queue, request) << std::endl;
+    // if (!WaitDie(request_queue, request)) {
+    //     std::cerr << "lock_IX_on_table: grant lock" << std::endl;
+    //     while (!GrantLock(request_queue, request)) {
+    //         request_queue.cv_.wait(lock);
+    //     }
+    //     txn->append_lock_set(lock_data_id);
+    // } else {
+    //     throw TransactionAbortException(txn->get_transaction_id(),
+    //                                     AbortReason::DEADLOCK_PREVENTION);
+    // }
 
     return true;
 }
@@ -203,33 +203,33 @@ bool LockManager::lock_IX_on_table(Transaction* txn, int tab_fd) {
  * @param {LockDataId} lock_data_id 要释放的锁ID
  */
 bool LockManager::unlock(Transaction* txn, LockDataId lock_data_id) {
-    std::unique_lock<std::mutex> lock(latch_);
-    auto it = lock_table_.find(lock_data_id);
-    if (it == lock_table_.end()) {
-        return false;
-    }
+    // std::unique_lock<std::mutex> lock(latch_);
+    // auto it = lock_table_.find(lock_data_id);
+    // if (it == lock_table_.end()) {
+    //     return false;
+    // }
 
-    auto& request_queue = it->second;
-    auto request_it = std::find_if(
-        request_queue.request_queue_.begin(),
-        request_queue.request_queue_.end(), [&](const LockRequest& req) {
-            return req.txn_id_ == txn->get_transaction_id();
-        });
+    // auto& request_queue = it->second;
+    // auto request_it = std::find_if(
+    //     request_queue.request_queue_.begin(),
+    //     request_queue.request_queue_.end(), [&](const LockRequest& req) {
+    //         return req.txn_id_ == txn->get_transaction_id();
+    //     });
 
-    if (request_it == request_queue.request_queue_.end()) {
-        return false;
-    }
+    // if (request_it == request_queue.request_queue_.end()) {
+    //     return false;
+    // }
 
-    request_queue.request_queue_.erase(request_it);
-    request_queue.group_lock_mode_ = CalculateGroupLockMode(request_queue);
-    request_queue.cv_.notify_all();
+    // request_queue.request_queue_.erase(request_it);
+    // request_queue.group_lock_mode_ = CalculateGroupLockMode(request_queue);
+    // request_queue.cv_.notify_all();
 
-    if (request_queue.request_queue_.empty()) {
-        lock_table_.erase(it);
-    }
+    // if (request_queue.request_queue_.empty()) {
+    //     lock_table_.erase(it);
+    // }
 
-    // 更新事务状态为 SHRINKING
-    txn->set_state(TransactionState::SHRINKING);
+    // // 更新事务状态为 SHRINKING
+    // txn->set_state(TransactionState::SHRINKING);
     return true;
 }
 
